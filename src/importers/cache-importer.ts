2582f211a473db60ce5f2c22bf9a22613169d915
joneff <joneff@gmail.com>
2022-12-07 11:24:13 +0200

    fix(cache-importer): only cache existing files

--- src/importers/cache-importer.ts
+++ src/importers/cache-importer.ts
@@ -1,37 +1,40 @@
+import fs from 'fs';
 import path from 'path';
 
 const EMPTY_IMPORT = {};
 
-export function sassCacheImporter(options: CacheImporterOptions = { cache: true }) : SassImporter {
+export function sassCacheImporter( options: CacheImporterOptions = { cache: true } ) : SassImporter {
 
     const cache = options.cache;
     const imported : Set<string> = cache instanceof Set
         ? cache
         : new Set();
 
-    function sassCacheImporter(url: string, prev: string) : null | LegacyImporterResult {
+    function sassCacheImporter( url: string, prev: string ) : null | LegacyImporterResult {
 
+        // If previous file is stdin, then we are importing from a string
         if (prev === 'stdin') {
             return null;
         }
 
-        if (url.startsWith('~')) {
-            imported.add(url);
-            return null;
-        }
-
-        const file = path.resolve(path.join(
+        const file = path.resolve(
             path.dirname(prev),
             url
-        ));
+        );
 
-        if (cache && imported.has(file)) {
-            return <LegacyImporterResult>EMPTY_IMPORT;
+        // Only cache existing files
+        if ( fs.existsSync( file ) ) {
+
+            if ( cache && imported.has( file ) ) {
+                return <LegacyImporterResult> EMPTY_IMPORT;
+            }
+
+            imported.add( file );
+
+            return { file };
         }
 
-        imported.add(file);
-
-        return { file };
+        return null;
     }
     sassCacheImporter.name = 'sassCacheImporter';
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
