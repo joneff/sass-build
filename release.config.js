10f49eb45c189f0bdc55f793034ce37a272750b0
joneff <joneff@gmail.com>
2023-01-06 15:32:10 +0200

    ci: add config file for semantic release

---
+++ release.config.js
@@ -0,0 +1,15 @@
+const config = {
+    branches: [
+        'develop'
+    ],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        '@semantic-release/git',
+        '@semantic-release/github'
+    ]
+};
+
+module.exports = config;
